80ef8b66e2ac88d01c5a44d3bd9f251dd0ebb872
AdrienVergnaud <a.vergnaud@gmail.com>
2022-01-26 17:17:04 +0100

    workflow terraform gcp

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/secrets/ema28-48-stochrsi-bot-99ffa9d7414e.json ema28-48-stochrsi-bot-99ffa9d7414e.json.gpg
